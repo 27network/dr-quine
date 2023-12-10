cf75aad6302622971c6eaf53fbf008a74fbfabee
xtrm <oss@xtrm.me>
2023-12-10 02:17:34 +0100

    ✨ feature(C/Sully): we ballin with this one

--- C/Sully.c
+++ C/Sully.c
@@ -4,17 +4,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-void error(){printf("error\n");exit(1);}
+void error(){printf("Error\n");exit(1);}
 int main(){
-	if(X <= 0)return 0;
-	char *file="Sully_"XS".c";
-	if(access(file, F_OK)!=0)error();
-	int fd=open(file,O_WRONLY|O_CREAT|O_TRUNC,0644);
-	if(fd<0)error();
-	dprintf(fd,"#define X %d\n#define XS \"%d\"\n",X-1,X-1);
-	dprintf(fd,"#include <stdio.h>\nint main(){printf(\"wow!\\n\");return 0;}");
-	close(fd);
-	if(!system("clang -Wall -Wextra -Werror -o Sully_"XS" Sully_"XS".c"))error();
-	if(!system("./Sully_"XS))error();
-	return 0;
-}
+if(X <= 0)return 0;
+char *file;
+asprintf(&file,"Sully_%d.c",X-1);
+int fd=open(file,O_WRONLY|O_CREAT|O_TRUNC,0644);
+free(file);
+if(fd<0)error();
+dprintf(fd,"#define X %d\n#define XS \"%d\"\n",X-1,X-1);
+dprintf(fd,"#include <stdio.h>\nint main(){printf(\"wow!\\n\");return 0;}");
+close(fd);
+if(system("clang -Wall -Wextra -Werror -o Sully_"XS" Sully_"XS".c"))error();
+if(system("./Sully_"XS))error();
+return 0;}
